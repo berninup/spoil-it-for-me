a19f76f2f2542ddc7ad74932c61abfd8c5994f80
berninup <benrincb@gmail.com>
2023-04-20 15:41:51 -0500

    Built proof of concept that outputs to console.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from "axios"
 
 function App() {
+  const [search, setSearch] = useState('');
+  const client = axios.create({
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`
+    }
+  })
+
+  const params = {
+    prompt: [`Can you give me notes on the real movie that matches ${search}, I want to discuss it as if I've seen it, give me all spoilers!`, `Give me the cast for the movie ${search}`, `Descripe 3 important scenes in ${search}`],
+    model: "text-davinci-003",
+    max_tokens: 1500,
+    temperature: 0.5,
+  }
+
+  
+
+  function handleChange(event) {
+    setSearch(event.target.value)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()    
+   client.post("https://api.openai.com/v1/completions", params).then((result) => { console.log(result.data.choices[0].text); console.log(result.data.choices)}).catch((err) => {console.log(err)})
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    <form on onSubmit={handleSubmit}>
+      <label>
+        Movie Search:
+      </label>
+      <input type='text' name="search" id="search" onChange={handleChange} />
+      <input type='submit' value="submit" />
+    </form>
+
+
     </div>
   );
 }
